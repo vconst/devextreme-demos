ed03a9fd5976d269724874449e2c8a6d83a18618
volnyagin <vconst@users.noreply.github.com>
2021-04-22 17:53:04 +0300

    Use test-code for e2e tests

--- e2e/e2e.test.js
+++ e2e/e2e.test.js
@@ -1,5 +1,39 @@
 import glob from 'glob';
 import { compareScreenshot } from './helpers/screenshort-comparer';
+import { ClientFunction } from 'testcafe';
+import { join } from 'path';
+import { existsSync, readFileSync } from 'fs';
+
+// const waitReadyState = ClientFunction(() => {
+//     return new Promise(resolve => {
+//         const interval = setInterval(() => {
+//             if(window.isReady) {
+//                 clearInterval(interval);
+//                 resolve();
+//             }
+//         }, 100);
+
+//         setTimeout(() => {
+//             clearInterval(interval);
+//             resolve();
+//         }, 180000);
+//     });
+// });
+
+const execCode = ClientFunction(code => {
+    const result = eval(code);
+    if(result && typeof result.then === 'function') {
+        return Promise.race([result, new Promise(resolve => setTimeout(30000, resolve))]);
+    } else {
+        return Promise.resolve();
+    }
+});
+
+const execTestCafeCode = async (t, code) => {
+    const testCafeFunction = eval(code);
+    return await testCafeFunction(t);
+}
+
 
 fixture`Getting Started`
     .beforeEach((t) => t.resizeWindow(800, 600));
@@ -11,18 +45,37 @@ const getDemoPaths = (platform) => {
 ['jQuery'/*, 'React', 'Vue', 'Angular'*/].forEach((approach) => {
     const demos = getDemoPaths(approach);
     console.log(demos);
-    // demos.length = 5;
 
     demos.forEach((demo) => {
-        const testName = demo.replace(/\//g, '-') + '-' + approach;
+        const testName = demo.replace(/\//g, '-');
         const testParts = demo.split("\/");
+        const widgetName = testParts[2];
+        const demoName = testParts[3];
+        const testCodePath = join(demo, '../test-code.js');
+        const testcafeTestCodePath = join(demo, '../testcafe-test-code.js');
+
+        if(demoName !== 'RightToLeftSupport') {
+            return;
+        }
         
         test
-            .page`http://127.0.0.1:8080/JSDemos/Demos/${testParts[2]}/${testParts[3]}/${approach}/`
+            .page`http://127.0.0.1:8080/JSDemos/Demos/${widgetName}/${demoName}/${approach}/`
             //.page`file://${path.resolve(demo)}/index.html`
             (testName, async t => {
+                // await waitReadyState();
+
+                if(existsSync(testCodePath)) {
+                    const code = readFileSync(testCodePath, 'utf8');
+                    await execCode(code);
+                }
+
+                if(existsSync(testcafeTestCodePath)) {
+                    const code = readFileSync(testcafeTestCodePath, 'utf8');
+                    await execTestCafeCode(t, code);
+                }
+
                 await t.expect(
-                    await compareScreenshot(t,  testParts[2] + "-" + testParts[3] + ".png")
+                    await compareScreenshot(t,  widgetName + "-" + demoName + ".png")
                 ).ok();
             });
     });
