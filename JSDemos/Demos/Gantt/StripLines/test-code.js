295db654fab8d2ef2b351580a7376a201a01d8c7
volnyagin <vconst@users.noreply.github.com>
2021-04-27 15:41:14 +0300

    Fix Gantt test

--- JSDemos/Demos/Gantt/StripLines/test-code.js
+++ JSDemos/Demos/Gantt/StripLines/test-code.js
@@ -1,1 +1,16 @@
-MockDate.set(new Date(2000, 3, 1, 0, 0, 0, 0));
+(function(factory) {
+    if (window.Promise && window.System) {
+        Promise.all([
+            System.import("devextreme/ui/gantt")
+        ]).then(function (args) {
+            factory(args[0]);
+        });
+    } else {
+        factory(DevExpress.ui.dxGantt);
+    }
+})(function(dxDateBox) {
+    $("#gantt").each(function (_, item) {
+        var instance = dxDateBox.getInstance(item);
+        instance.option("stripLines[2].start", new Date(2000, 3, 1, 0, 0, 0, 0));
+    });
+});
