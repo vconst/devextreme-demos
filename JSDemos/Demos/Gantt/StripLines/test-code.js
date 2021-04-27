96c6a789b6c2fd4bd59771ea4f2f6f89cd715c94
volnyagin <vconst@users.noreply.github.com>
2021-04-27 15:44:57 +0300

    simplify test-code for gantt

--- JSDemos/Demos/Gantt/StripLines/test-code.js
+++ JSDemos/Demos/Gantt/StripLines/test-code.js
@@ -8,9 +8,7 @@
     } else {
         factory(DevExpress.ui.dxGantt);
     }
-})(function(dxDateBox) {
-    $("#gantt").each(function (_, item) {
-        var instance = dxDateBox.getInstance(item);
-        instance.option("stripLines[2].start", new Date(2000, 3, 1, 0, 0, 0, 0));
-    });
+})(function(Gantt) {
+    var instance = Gantt.getInstance(document.getElementById("gantt"));
+    instance.option("stripLines[2].start", new Date(2000, 3, 1, 0, 0, 0, 0));
 });
